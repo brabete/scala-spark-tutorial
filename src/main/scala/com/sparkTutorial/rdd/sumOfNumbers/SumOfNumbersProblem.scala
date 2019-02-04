f7eb6e1701797cce3ddd66302e0f2ff88a4b783a
Andrei Brabete <andreibrabete@gmail.com>
2019-02-04 11:26:58 +0000

    update

--- src/main/scala/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.scala
+++ src/main/scala/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.scala
@@ -1,5 +1,8 @@
 package com.sparkTutorial.rdd.sumOfNumbers
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
 object SumOfNumbersProblem {
 
   def main(args: Array[String]) {
@@ -9,5 +12,22 @@ object SumOfNumbersProblem {
 
        Each row of the input file contains 10 prime numbers separated by spaces.
      */
+
+    Logger.getLogger("org").setLevel(Level.OFF)
+
+    val conf = new SparkConf().setAppName("sumOfNum").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    val primeNum = sc.textFile("in/prime_nums.text")
+
+    val numbers = primeNum.flatMap(line => line.split("\\s+"))
+
+    val validNumbers = numbers.filter(number => !number.isEmpty)
+
+    val intNumbers = validNumbers.map(number => number.toInt)
+
+    val sumOfNumbers = intNumbers.reduce((x, y) => x + y)
+
+    println("Sum of Numbers: " + sumOfNumbers)
   }
 }
